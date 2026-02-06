b6b793c4e87c6c5b3a9785c1266f1f6aa593ae9c
Link4Electronics <link4electronics@gmail.com>
2026-02-06 10:56:13 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,8 @@
 set -eu
 
 ARCH=$(uname -m)
-export ARCH
+VERSION=$(pacman -Q corsix-th-git | awk '{print $2; exit}') # example command to get version of application here
+export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
