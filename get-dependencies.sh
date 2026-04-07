c8fbd2f655f5ead35f1a714da695f9c8cb7d1961
Link4Electronics <link4electronics@gmail.com>
2026-04-07 19:16:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,17 +7,16 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake          \
-    doxygen        \
-    fluidsynth     \
-    libdecor       \
-    pipewire-alsa  \
-    pipewire-audio \
-    pipewire-jack  \
-    rtmidi         \
-    sdl2_mixer     \
+    cmake           \
+    doxygen         \
+    fluidsynth      \
+    libdecor        \
+    pipewire-alsa   \
+    pipewire-audio  \
+    pipewire-jack   \
+    rtmidi          \
+    sdl2_mixer      \
     soundfont-fluid
-    #timidity++
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -27,6 +26,8 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 
 # If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/share/soundfonts
+cp /usr/share/soundfonts/FluidR3_GM.sf2 ./AppDir/share/soundfonts
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
     echo "Making nightly build of CorsixTH..."
     echo "---------------------------------------------------------------"
@@ -40,11 +41,12 @@ else
     git clone --branch "$VERSION" --single-branch "$REPO" ./CorsixTH
     echo "${VERSION#v}" > ~/version
 
-    pacman -S --noconfirm lua54 lua54-filesystem lua54-lpeg
+    pacman -S --noconfirm lua54 lua54-filesystem lua54-lpeg timidity++
+    mkdir -p ./AppDir/bin
+    cp /etc/timidity/timidity.cfg ./AppDir/bin
+    
     cd ./CorsixTH
     cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_UNIT_TESTS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DLUA_PROGRAM_PATH=/usr/bin/lua5.4 -DLUA_INCLUDE_DIR=/usr/include/lua5.4 -DLUA_LIBRARY=/usr/lib/liblua5.4.so .
     cd CorsixTH && make -j$(nproc)
     make install
 fi
-mkdir -p ./AppDir/share/soundfonts
-cp /usr/share/soundfonts/FluidR3_GM.sf2 ./AppDir/share/soundfonts
