ff8458ecfc638d4a26045092739eb327629ea185
Link4Electronics <link4electronics@gmail.com>
2026-04-07 16:06:14 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,16 +15,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/corsix-th \
-/usr/lib/lua/5.5/lpeg.so
-#/usr/lib/alsa-lib/libasound_module_pcm_a52.so \
-#/usr/lib/alsa-lib/libasound_module_pcm_jack.so \
-#/usr/lib/alsa-lib/libasound_module_pcm_oss.so \
-#/usr/lib/alsa-lib/libasound_module_pcm_pipewire.so \
-#/usr/lib/alsa-lib/libasound_module_pcm_speex.so \
-#/usr/lib/alsa-lib/libasound_module_pcm_upmix.so \
-#/usr/lib/alsa-lib/libasound_module_pcm_usb_stream.so \
-#/usr/lib/alsa-lib/libasound_module_pcm_vdownmix.so
+quick-sharun /usr/bin/corsix-th /usr/lib/lua/5.5/lpeg.so
 
 # Additional changes can be done in between here
 
