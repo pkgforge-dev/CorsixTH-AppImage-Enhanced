654db8a411c1a6e3fa4ae8711307864094041835
Link4Electronics <link4electronics@gmail.com>
2026-04-07 16:35:43 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cake           \
+    cmake          \
     doxygen        \
     fluidsynth     \
     libdecor       \
