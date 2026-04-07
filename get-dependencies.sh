2d4d9a1ca1f94649718a34524bed4b1cc5c761c5
Link4Electronics <link4electronics@gmail.com>
2026-04-07 17:32:00 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -42,7 +42,8 @@ else
     pacman -S --noconfirm lua54 lua54-filesystem lua54-lpeg
     mkdir -p ./AppDir/bin
     cd ./CorsixTH
-    cmake -DCMAKE_BUILD_TYPE=Release -DLUA_PROGRAM_PATH=/usr/bin/lua5.4 -DLUA_INCLUDE_DIR=/usr/include/lua5.4 -DLUA_LIBRARY=/usr/lib/liblua5.4.so -CMAKE_SOURCE_DIR=./CorsixTH.lua .
+    cp -v CorsixTH/CorsixTH.lua ./AppDir/bin
+    cmake -DCMAKE_BUILD_TYPE=Release -DLUA_PROGRAM_PATH=/usr/bin/lua5.4 -DLUA_INCLUDE_DIR=/usr/include/lua5.4 -DLUA_LIBRARY=/usr/lib/liblua5.4.so -CMAKE_SOURCE_DIR=./AppDir/bin/CorsixTH.lua .
     make -j$(nproc)
-    mv -v CorsixTH/Bitmap CorsixTH/Campaigns CorsixTH/Graphics CorsixTH/Levels CorsixTH/Lua CorsixTH/Luatest CorsixTH/CorsixTH.lua CorsixTH/corsix-th ../AppDir/bin
+    mv -v CorsixTH/Bitmap CorsixTH/Campaigns CorsixTH/Graphics CorsixTH/Levels CorsixTH/Lua CorsixTH/Luatest CorsixTH/corsix-th ../AppDir/bin
 fi
