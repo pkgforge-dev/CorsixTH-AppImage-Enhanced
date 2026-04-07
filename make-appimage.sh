4fcc431aa760847103b72ce376d88b22676e9fc7
Link4Electronics <link4electronics@gmail.com>
2026-04-07 17:58:48 -0300

    add stable

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,19 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q corsix-th-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/scalable/apps/corsix-th.svg
-export DESKTOP=/usr/share/applications/com.corsixth.corsixth.desktop
+export ICON=https://raw.githubusercontent.com/CorsixTH/CorsixTH/refs/heads/master/CorsixTH/Original_Logo.svg
+export DESKTOP=https://raw.githubusercontent.com/CorsixTH/CorsixTH/refs/heads/master/CorsixTH/com.corsixth.corsixth.desktop
 export STARTUPWMCLASS=corsix-th
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/corsix-th /usr/lib/lua/5.5/lpeg.so
+quick-sharun /usr/bin/corsix-th /usr/lib/lua/*/lpeg.so
 
 # Additional changes can be done in between here
 
