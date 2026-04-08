5b58bcc0360d904f816ddb413656342a4e098150
Link4Electronics <link4electronics@gmail.com>
2026-04-08 18:16:15 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,15 +28,30 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 # If the application needs to be manually built that has to be done down here
 mkdir -p ./AppDir/share/soundfonts
 cp /usr/share/soundfonts/FluidR3_GM.sf2 ./AppDir/share/soundfonts
+REPO="https://github.com/CorsixTH/CorsixTH"
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
     echo "Making nightly build of CorsixTH..."
     echo "---------------------------------------------------------------"
-    make-aur-package corsix-th-git
-    VERSION=$(pacman -Q corsix-th-git | awk '{print $2; exit}')
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone "$REPO" ./CorsixTH
+    echo "$VERSION" > ~/version
+
+    pacman -S --noconfirm lua lua-filesystem lua-lpeg
+    mkdir -p ./AppDir/bin
+    cd ./CorsixTH
+    cmake . \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DENABLE_UNIT_TESTS=OFF \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DLUA_PROGRAM_PATH=/usr/bin/lua \
+        -DLUA_INCLUDE_DIR=/usr/include/lua \
+        -DLUA_LIBRARY=/usr/lib/liblua5.5.so
+
+    cd CorsixTH && make -j$(nproc)
+    make install
 else
     echo "Making stable build of CorsixTH..."
     echo "---------------------------------------------------------------"
-    REPO="https://github.com/CorsixTH/CorsixTH"
     VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//')"
     git clone --branch "$VERSION" --single-branch "$REPO" ./CorsixTH
     echo "${VERSION#v}" > ~/version
