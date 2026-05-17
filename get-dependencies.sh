111bee4726afb99eaed0c5b89879fa3f33a7e743
Link4Electronics <link4electronics@gmail.com>
2026-05-17 11:08:46 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,26 +7,23 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	cmake           \
-	doxygen         \
-	fluidsynth      \
-	libdecor        \
-	pipewire-alsa   \
-	pipewire-audio  \
-	pipewire-jack   \
-	rtmidi          \
-	sdl2_mixer      \
+	cmake          \
+	doxygen        \
+	fluidsynth     \
+	pipewire-alsa  \
+	pipewire-audio \
+	pipewire-jack  \
+	rtmidi         \
+	sdl2_mixer     \
+	sdl3_mixer	   \
 	timidity++
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini libdecor-mini
 
-# Comment this out if you need an AUR package
-# make-aur-package zenity-rs-bin
-
-# If the application needs to be manually built that has to be done down here
-git clone https://github.com/CorsixTH/CorsixTH ./corsixth
+REPO="https://github.com/CorsixTH/CorsixTH"
+git clone --depth 1 "$REPO" ./corsixth
 cd ./corsixth
 
 set --
@@ -40,7 +37,7 @@ else
 	echo "---------------------------------------------------------------"
 	pacman -S --noconfirm lua54 lua54-filesystem lua54-lpeg
 	git fetch --tags origin
-	TAG=$(git tag --sort=-v:refname | grep -vi 'rc\|alpha' | head -1)
+	TAG=$(git tag --sort=-v:refname | grep -vi 'rc\|alpha\|beta' | head -1)
 	git checkout "$TAG"
 	echo "$TAG" > ~/version
 	set -- \
@@ -49,14 +46,10 @@ else
 		-DLUA_LIBRARY=/usr/lib/liblua5.4.so
 fi
 
-mkdir ./build
-cd ./build
-
-cmake ../ \
+cmake ./ -B build \
 	-DCMAKE_BUILD_TYPE=Release  \
 	-DENABLE_UNIT_TESTS=OFF     \
 	-DCMAKE_INSTALL_PREFIX=/usr \
 	"$@"
-
-make -j$(nproc)
-make install
+cmake --build build -j$(nproc)
+cmake --install build
